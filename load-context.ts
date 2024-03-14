bcebb0bc8c9eb1ee23a05d2fa43975415ab9fe38
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-14 19:37:42 -0300

    add tailwind

--- load-context.ts
+++ load-context.ts
@@ -2,12 +2,6 @@ import { EnvSchema } from "envs/env";
 import { type PlatformProxy } from "wrangler";
 import { z } from "zod";
 
-// When using `wrangler.toml` to configure bindings,
-// `wrangler types` will generate types for those bindings
-// into the global `Env` interface.
-// Need this empty interface so that typechecking passes
-// even if no `wrangler.toml` exists.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Env {}
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;

c0188f23461ec0508eb7cb5c60e637ef6fbc1147
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-14 13:14:12 -0300

    config load-context

--- load-context.ts
+++ load-context.ts
@@ -1,4 +1,6 @@
+import { EnvSchema } from "envs/env";
 import { type PlatformProxy } from "wrangler";
+import { z } from "zod";
 
 // When using `wrangler.toml` to configure bindings,
 // `wrangler types` will generate types for those bindings
@@ -9,9 +11,19 @@ import { type PlatformProxy } from "wrangler";
 interface Env {}
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
-
+export type AppEnvs = z.infer<typeof EnvSchema>;
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
+  }
+}
+
+declare module "@remix-run/cloudflare" {
+  interface LoaderFunctionArgs {
+    context: {
+      cloudflare: {
+        env: AppEnvs;
+      };
+    };
   }
 }

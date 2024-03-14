15391eb738f746226827568765dd4114ce2136d6
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-14 13:09:02 -0300

    Remove unused code and update Vite configuration

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,9 +1,7 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
 import * as build from "../build/server";
-import { getLoadContext } from "envs/load-context";
 
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
-import { getLoadContext } from "envs/load-context";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    remixCloudflareDevProxy({ getLoadContext }),
-    remix(),
-    tsconfigPaths(),
-  ],
+  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
